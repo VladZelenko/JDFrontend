f84d64339ac2e11cfea9414eaf5a9824ac91a423
VladZelenko <Vlad6555@ukr.net>
2017-04-13 19:15:19 +0300

    markup contact

--- wp-content/themes/jdfrontend/page-contact-us.php
+++ wp-content/themes/jdfrontend/page-contact-us.php
@@ -14,18 +14,37 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-		<div class="container">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="container contact">
 			<div class="row">
-				<div class="col-sm-12 col-md-6 col-lg-6">
+				<div class="col-sm-12 col-md-6 col-lg-6 contact-form">
 					<?php echo do_shortcode('[contact-form-7 id="112" title="Contact form 1"]'); ?>
 				</div>
-				<div class="col-sm-12 col-md-6 col-lg-6"></div>
+				<div class="col-sm-12 col-md-6 col-lg-6 contact-info">
+					<h2 class="title"><?php echo get_theme_mod(''); ?></h2>
+					<ul>
+						<li>
+							<a href="mailto:<?php echo get_theme_mod(''); ?>">
+								<i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_theme_mod(''); ?>
+							</a>
+						</li>
+						<li>
+							<a href="tel:<?php echo get_theme_mod(''); ?>">
+								<i class="fa fa-mobile" aria-hidden="true"></i><?php echo get_theme_mod(''); ?>
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo get_theme_mod(''); ?>">
+								<i class="fa fa-globe" aria-hidden="true"></i><?php echo get_theme_mod(''); ?>
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer();
